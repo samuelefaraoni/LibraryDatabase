d956154f45ae5f6f06486eb498182b509dda7b19
samuelefaraoni <samuele.faraoni.stud@ispascalcomandini.it>
2026-09-07 16:28:20 +0200

    Added class MediaCollector

---
+++ src/LibraryDatabase.Domain/MediaCollector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryDatabase.Domain
+{
+    public class MediaCollector
+    {
+        public List<AbstractMedia> Collector;
+
+        public MediaCollector()
+        {
+            Collector = new List<AbstractMedia>();
+        }
+
+        private void AddMedia(AbstractMedia media)
+        {
+            Collector.Add(media);
+        }
+
+        private void RemoveMedia(AbstractMedia media)
+        {
+            Collector.Remove(media);
+        }
+    }
+}
